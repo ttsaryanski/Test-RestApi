c551346c9ecf206e87fee3afe8db1cebdec3b9dd
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-12-22 09:52:25 +0200

    authMiddleware editet

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from "../lib/jwt.js";
-import { JWT_SECRET } from "../config/constans.js";
+//import { JWT_SECRET } from "../config/constans.js";
 import InvaliToken from "../models/InvalidToken.js";
 
 const authMiddleware = async (req, res, next) => {
@@ -15,7 +15,7 @@ const authMiddleware = async (req, res, next) => {
       return res.status(403).send({ message: "Invalid token!" }).end();
     }
 
-    const decodedToken = await jwt.verify(token, JWT_SECRET);
+    const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
 
     req.user = decodedToken;
     req.isAuthenticated = true;
